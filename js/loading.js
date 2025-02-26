e77361ffa4ba6bdf384f04b392d3776b923d0395
joseph4808 <93751535+joseph4808@users.noreply.github.com>
2025-02-26 09:40:56 -0800

    text

---
+++ js/loading.js
@@ -0,0 +1,24 @@
+const loadText = document.querySearch(".loading-text");
+const bg = document.querySearch(".bg");
+
+let load = 0;
+let int = setInterval(blurring, 30);
+
+blurring();
+blurring();
+
+
+function blurring(){
+    ++load;
+    if(load >=100){
+        clearInterval(int);
+    }
+    loadText.innerText = `${load}%`;
+    bg.style.filter = `blur(${50 - (load / 2)}px)`;
+    loadText.style.opacity = `${1 - (load / 100)}`;
+
+
+
+}
+
+
