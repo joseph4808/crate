c2533b716f4d0809b34cf1495e9b2e136220a577
joseph4808 <93751535+joseph4808@users.noreply.github.com>
2025-02-27 18:23:15 +0000

    Thu, Feb 27, 2025, 10:23 AM -08:00

--- js/buttons.js
+++ js/buttons.js
@@ -3,5 +3,5 @@ const tRButton = document.querySlect(".tbTwo");
 const bLButton = document.querySlect(".bbOne");
 const bRButton = document.querySlect(".bbTwo");
 
-tLButton.style.background = "red"
+tLButton.style.background-color = "red"
 
