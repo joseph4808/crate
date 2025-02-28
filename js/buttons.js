f4fc84383a1df1d8320c119235b3f811e2b364fd
joseph4808 <93751535+joseph4808@users.noreply.github.com>
2025-02-28 17:04:22 +0000

    Fri, Feb 28, 2025, 9:04 AM -08:00

--- js/buttons.js
+++ js/buttons.js
@@ -3,19 +3,21 @@ const tRButton = document.querySelector(".tbTwo");
 const bLButton = document.querySelector(".bbOne");
 const bRButton = document.querySelector(".bbTwo");
 
-let num1 = 0;
+let num1 = 1;
 let color1 = "white";
 
 tLButton.onclick = changeColor(num1, color1);
 
 function changeColor(num, color){
-    num++;
+
     switch(num){
         case 1:
             color = "red";
+            num++;
             break;
         case 2:
             color = "blue";
+            num++;
             break;
         case 3:
             color = "green";
