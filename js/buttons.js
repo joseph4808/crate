1b92c5e6ec2a8fe18b75291dc393cd55c333803f
joseph4808 <93751535+joseph4808@users.noreply.github.com>
2025-03-07 17:54:19 +0000

    Fri, Mar 7, 2025, 9:54 AM -08:00

--- js/buttons.js
+++ js/buttons.js
@@ -95,11 +95,11 @@ function returnGameComplete(){
 function gameComplete(){
     completeDis.style.zIndex = "100";
     completeDis.style.height = "80vh";
-    completeDis.style.width = "80vw";
+    completeDis.style.width = "100vw";
     completeDis.style.opacity = ".8";
-    yay.innerHTML = "YOU DID IT!!!!!"
+    yay.innerHTML = "YOU DID IT!!!!!";
     yay.style.opacity = 0;
     yay.style.opacity = 1;
     yay.style.top = "50px";
-    yay.style.left = "100px";
+    yay.style.left = "200px";
 }
