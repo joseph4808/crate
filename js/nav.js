357ec53ff35af68631a0bdf1d9d14d280a7b070f
joseph4808 <93751535+joseph4808@users.noreply.github.com>
2025-03-14 18:48:20 +0000

    Fri, Mar 14, 2025, 11:48 AM -07:00

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,7 @@
 const nav = document.querySelector(".nav");
 
 window.addEventListener("scroll", ()=>{
-    if(window.scrollY > 100){
+    if(window.scrollY > 60){
         nav.classList.add("active");
     } else {
         nav.classList.remove("active")
