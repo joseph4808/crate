d209ebae53c6e2e1a1d1a3d77ea930d231cd8604
joseph4808 <93751535+joseph4808@users.noreply.github.com>
2025-03-14 18:23:15 +0000

    Fri, Mar 14, 2025, 11:23 AM -07:00

--- js/game.js
+++ js/game.js
@@ -7,12 +7,12 @@ let numbers = [0, 0, 0, 0];
 let numberLog = [];
 
 button.forEach((buttons)=>{
-    const number = document.getElementById(`btn${++btnIncrement}`)
+    const number = document.getElementById(`${++btnIncrement}btn`)
     buttons = number;
     numbers[btnIncrement-1] = getRndInteger(1, 9);
     buttons.innerHTML = numbers[btnIncrement-1];
     buttons.addEventListener("click", ()=>{
-        numberLog.push(numbers[parseInt(buttons - "btn")-1]);
+        numberLog.push(numbers[parseInt(buttons)-1]);
         changeNumbers();
         })
     })
