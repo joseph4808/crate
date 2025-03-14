8f1673246c2c092ea59a4011a4cf83652aa53e21
joseph4808 <93751535+joseph4808@users.noreply.github.com>
2025-03-14 15:43:17 +0000

    Fri, Mar 14, 2025, 8:43 AM -07:00

---
+++ js/game.js
@@ -0,0 +1,27 @@
+const button = ["", "", "", ""];
+
+let btnIncrement = 0;
+let btnIncrementTwo = 0;
+let numbers = [0, 0, 0, 0]
+let numLog = [];
+
+button.forEach((buttons)=>{
+    const number = document.getElementById(`btn${++btnIncrement}`)
+    buttons = number;
+    numbers[btnIncrement-1] = getRndInteger(1, 9);
+    buttons.innerHTML = numbers[btnIncrement-1];
+    buttons.addEventListener("click", ()=>{
+        numLog.push(numbers[parseInt(buttons)-1]);
+        button.forEach((btn)=>{
+            numbers[btnIncrementTwo] = getRndInteger(1, 9);
+            btn.innerHTML = numbers[btnIncrementTwo++];
+        })
+    })
+});
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
