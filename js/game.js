a5db2719975d54917a013fc6647cdd254faf25a9
joseph4808 <93751535+joseph4808@users.noreply.github.com>
2025-03-14 15:49:56 +0000

    Fri, Mar 14, 2025, 8:49 AM -07:00

--- js/game.js
+++ js/game.js
@@ -1,4 +1,5 @@
 const button = ["", "", "", ""];
+const text = document.querySelector(".text");
 
 let btnIncrement = 0;
 let btnIncrementTwo = 0;
@@ -11,7 +12,8 @@ button.forEach((buttons)=>{
     numbers[btnIncrement-1] = getRndInteger(1, 9);
     buttons.innerHTML = numbers[btnIncrement-1];
     number.addEventListener("click", ()=>{
-        numLog.push(numbers[parseInt(buttons)-1]);
+        numLog.push(numbers[parseInt(number)-1]);
+        text.innerHTML = numlog;
         button.forEach((btn)=>{
             numbers[btnIncrementTwo] = getRndInteger(1, 9);
             btn.innerHTML = numbers[btnIncrementTwo++];
