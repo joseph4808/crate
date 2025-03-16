c8552c3d68701bbf3bc56b1fa453191c54b3b504
joseph4808 <93751535+joseph4808@users.noreply.github.com>
2025-03-16 17:08:20 +0000

    Sun, Mar 16, 2025, 10:08 AM -07:00

--- js/game.js
+++ js/game.js
@@ -6,3 +6,15 @@ let btnIncrementTwo = 0;
 let numbers = [0, 0, 0, 0];
 let numberLog = [];
 
+button.forEach((buttons)=>{
+    const number = document.getElementById(`${++btnIncrement}btn`)
+    buttons = number;
+    });
+
+
+
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
