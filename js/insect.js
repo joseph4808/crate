718356be6843b2c7770c027be5865c904acf0b51
joseph4808 <93751535+joseph4808@users.noreply.github.com>
2025-03-28 17:07:58 +0000

    Fri, Mar 28, 2025, 10:07 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,4 +1,18 @@
 const screens = document.querySelectorAll(".screen");
+const chooseInsectBtns = document.querySelectorAll(".choose-insect-btn");
+const gameContainer = document.getElementById("game-container");
+const startBtn =  document.getElementById("start-btn")
+const timeEl = document.getElementById("time");
+const scoreEl = document.getElementbyId("score");
+const message = document.getElementById("message");
+
+let seconds = 0;
+let score = 0;
+let selectedInsect = {}
+
+startBtn.addEventListener("click" ()=>{
+    screens[0].classList.add("up");
+})
 
 //screens[0].classList.add("up");
 
