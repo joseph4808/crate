8c3c072eed5e686f3d11737e31c9d89a6df3727e
joseph4808 <93751535+joseph4808@users.noreply.github.com>
2025-04-04 16:56:48 +0000

    Fri, Apr 4, 2025, 9:56 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -1,11 +1,18 @@
 const ball = document.createElement("div");
+var ballRadius = 70
+const windowHeight = window.innerHeight;
+const windowWidth = window.innerWidth;
+
 
 createBall();
 
 function createBall(){
     document.body.appendChild(ball)
-    ball.style.height = "100px"
-    ball.style.width = "100px"
+    ball.style.height = `${ballRadius*2}px`
+    ball.style.width = `${ballRadius*2}px`
     ball.style.borderRadius = "50%"
     ball.style.backgroundColor = "lightgreen"
+    ball.style.position= "absolute"
+    ball.style.top = `${(windowHeight/2) - ballRadius}px`;
+    ball.style.left = `${(windowWidth/2) - ballRadius}px`;
 }
