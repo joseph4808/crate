f2094ca5b4fa13cd1e663aab4da266a79c02de5c
joseph4808 <93751535+joseph4808@users.noreply.github.com>
2025-05-16 17:16:35 +0000

    Fri, May 16, 2025, 10:16 AM -07:00

--- js/speech.js
+++ js/speech.js
@@ -7,32 +7,58 @@ const closeBtn = document.getElementById("close");
 
 const data = [
     {
-        image: "../drink.jpg"
-        text: "Im Thirsty"
+        image: "../drink.jpg",
+        text: "I'm Thirsty"
     },
     {
-        image: "../food.jpg"
-        text: "Im Hungry"
+        image: "../food.jpg",
+        text: "I'm Hungry"
     },
     {
-        image: "../tired.jpg"
-        text: "Im Tired"
+        image: "../tired.jpg",
+        text: "I'm Tired"
     },
     {
-        image: "../hurt.jpg"
-        text: "Im Hurt"
+        image: "../hurt.jpg",
+        text: "I'm Hurt"
     },
     {
-        image: "../happy.jpg"
-        text: "Im Happy"
+        image: "../happy.jpg",
+        text: "I'm Happy"
     },
     {
-        image: "../angry.jpg"
-        text: "Im Angry"
+        image: "../angry.jpg",
+        text: "I'm Angry"
     },
     {
-        image: "../sad.jpg"
-        text: "Im Sad"
+        image: "../sad.jpg",
+        text: "I'm Sad"
+    },
+    {
+        image: "../scared.jpg",
+        text: "I'm Scared"
+    },
+    {
+        image: "../outside.jpg",
+        text: "I Want To Go Outside"
+    },
+    {
+        image: "../home.jpg",
+        text: "I Want To Go Home"
+    },
+    {
+        image: "../school.jpg",
+        text: "I Want To Go To School"
+    },
+    {
+        image: "../grandma.jpg",
+        text: "I Want To Go To Grandma's"
     },
 
 ]
+
+data.forEach(data, createBox(data))
+
+function createBox(item){
+    console.log(item);
+}
