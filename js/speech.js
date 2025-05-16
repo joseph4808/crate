63fbd8d5d10e35ce2f543f332a8490f9cf07a8b9
joseph4808 <93751535+joseph4808@users.noreply.github.com>
2025-05-16 17:09:14 +0000

    Fri, May 16, 2025, 10:09 AM -07:00

---
+++ js/speech.js
@@ -0,0 +1,38 @@
+const main = document.queryselector("main");
+const voicesSelect = document.getElementById("voices");
+const textarea = document.getElementById("text");
+const readBtn = document.getElementById('read');
+const toggleBtn = document.getElementById('toggle');
+const closeBtn = document.getElementById("close");
+
+const data = [
+    {
+        image: "../drink.jpg"
+        text: "Im Thirsty"
+    },
+    {
+        image: "../food.jpg"
+        text: "Im Hungry"
+    },
+    {
+        image: "../tired.jpg"
+        text: "Im Tired"
+    },
+    {
+        image: "../hurt.jpg"
+        text: "Im Hurt"
+    },
+    {
+        image: "../happy.jpg"
+        text: "Im Happy"
+    },
+    {
+        image: "../angry.jpg"
+        text: "Im Angry"
+    },
+    {
+        image: "../sad.jpg"
+        text: "Im Sad"
+    },
+
+]
